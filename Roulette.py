48cf0fcf57560b135374f26a725b48fdf597c8c6
William Webb <webbdevvue@gmail.com>
2018-11-17 22:13:08 -0500

    added table and ball spin play
    - added the table with the specific colored and numbered spots
    - added the ball play fuctionality that lands on a specific number and color

--- Roulette.py
+++ Roulette.py
@@ -1,13 +1,47 @@
+"""
+WELCOME TO THE BIG WHEEL'S CASINO & CABARET
+
+Tasks:
+[] Set up the table with proper numbers
+[] 
+"""
+
 import random
+#dice_one = random.randint(0, 36)
 
-dice_one = random.randint(0, 36)
 
-listy = []
-for n in range(0, 37):
-  listy.append(n)
+'''
+setup table spaces
+'''
 
-table = [0, 1, 2, 3, 4, 22, 23, 24, 25]
+zero_spot = {
+    'zero': [0, 0]
+    }
+red_spot = {
+    'red': [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    }
+black_spot = {
+    'black': [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+    }
 
-ball = random.choice(table)
-print(listy)
-print(ball)
+
+"""
+ball played
+"""
+
+ball = random.choice(zero_spot['zero'] + red_spot['red'] + black_spot['black'])
+
+'''
+Determine what spot the ball landed on
+'''
+
+
+if ball in [x for y in zero_spot.values() for x in y]:
+    print(f'ball lands on GREEN: {ball}')
+elif ball in [x for y in red_spot.values() for x in y]:
+    print(f'ball lands on RED: {ball}')
+elif ball in [x for y in black_spot.values() for x in y]:
+    print(f'ball lands on BLACK: {ball}')
+
+    
+
